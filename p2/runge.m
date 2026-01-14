1069cf915c93431c671aedc8bec2985795582e3b
karolsek12 <karol.sek12@wp.pl>
2026-01-14 09:28:16 +0100

    smecta

--- p2/runge.m
+++ p2/runge.m
@@ -1,4 +1,21 @@
 function y = runge(b, a, x0, xN, y0, N,table)
+% Projekt 2, zadanie 47
+% Karol Sęk, 334831
+% 
+% Wyznaczanie wartości funkcji będącej rozwiązaniem 
+% równiania różniczkowego liniowego dowolnego rzędu 
+% metodą Rungego-Kutty rzędu 4-go, wzór 3/8.
+% WEJŚCIE
+%   b - uchwyt do funkcji będącej wyrazem wolnym równania różniczkowego
+%   a - tablica komórkowa uchwytów do funkcji, przy czym 
+%   a{i} - funkcja przy (i-1) pochodnej funkcji   
+%   x0 - lewe ograniczenie przedziału na którym wyznaczane 
+%   będą wartości funkcji 
+%   xN - prawe ograniczenie przedziału na którym wyznaczane 
+%   będą wartości funkcji
+%   y0 - wektor wartości funkcji oraz kolejnych pochodnych w punkcjie x0
+%   N - liczba podprzedziałow na który będzie podzielony przedział <x0,xN>
+%   table - 
 
 if nargin < 7
     table = 0;
